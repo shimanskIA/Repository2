e6fc687196ca55c88e2f8290cf6263b15cc1126a
Ivan Shimanski <ivan_shi@list.ru>
2019-10-17 20:25:20 +0300

    Функция записи в специальный файл

---
+++ GornerTableModel3.java
@@ -0,0 +1,104 @@
+package Package3;
+
+import javax.swing.table.AbstractTableModel;
+
+public class GornerTableModel extends AbstractTableModel
+{
+    private Double[] coefficients;
+    private Double from;
+    private Double to;
+    private Double step;
+    public GornerTableModel(Double from, Double to, Double step,  Double[] coefficients)
+    {
+        this.from = from;
+        this.to = to;
+        this.step = step;
+        this.coefficients = coefficients;
+    }
+    public Double getFrom()
+    {
+        return from;
+    }
+    public Double getTo()
+    {
+        return to;
+    }
+    public Double getStep()
+    {
+        return step;
+    }
+    public int getColumnCount()
+    {
+        return 4;
+    }
+        public int getRowCount()
+        {
+            return new Double(Math.ceil((to - from) / step)).intValue() + 1;
+        }
+            public Object getValueAt(int row, int col)
+            {
+                double x = from + step * row;
+                if (col == 0)
+                {
+                    return x;
+                }
+                if (col == 1)
+                    {
+                        Double result = 0.0;
+                        Double res1 = x * (x * coefficients[0] + coefficients[1]);
+                        for (int i = 2; i + 1 < coefficients.length; i++)
+                        {
+                           result += x * (res1 + coefficients[i]);
+                           res1 = result;
+                        }
+                        result += coefficients[coefficients.length - 1];
+                        return result;
+                    }
+                if (col == 2)
+                {
+                    Double result = 0.0;
+                    Double res1 = x * (x * coefficients[coefficients.length - 1] + coefficients[coefficients.length - 2]);
+                    for (int i = coefficients.length - 3; i > 0; i--)
+                    {
+                        result += x * (res1 + coefficients[i]);
+                        res1 = result;
+                    }
+                    result += coefficients[0];
+                    return result;
+                }
+                else
+                {
+                    Double result1 = 0.0;
+                    Double res11 = x * (x * coefficients[0] + coefficients[1]);
+                    for (int i = 2; i + 1 < coefficients.length; i++)
+                    {
+                        result1 += x * (res11 + coefficients[i]);
+                        res11 = result1;
+                    }
+                    result1 += coefficients[coefficients.length - 1];
+                    Double result2 = 0.0;
+                    Double res12 = x * (x * coefficients[coefficients.length - 1] + coefficients[coefficients.length - 2]);
+                    for (int i = coefficients.length - 3; i > 0; i--)
+                    {
+                        result2 += x * (res12 + coefficients[i]);
+                        res12 = result2;
+                    }
+                    result2 += coefficients[0];
+                    return result1 - result2;
+                }
+            }
+            public String getColumnName(int col)
+            {
+                switch (col)
+                {
+                    case 0: return "Значение X";
+                    case 1: return "Значение многочлена";
+                    case 2: return "Подсчет при других коэффициентах";
+                    default: return "Разница";
+                }
+            }
+            public Class<?> getColumnClass(int col)
+            {
+                return Double.class;
+            }
+}
